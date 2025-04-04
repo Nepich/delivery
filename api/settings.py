79eb60cf76473d1efb62043918774271bedcadf4
NEPOCHATYKH Dmitry A <dmitry.nepochatykh@raiffeisen.ru>
2025-04-04 09:04:34 +0300

    Задание 8

--- api/settings.py
+++ api/settings.py
@@ -1,14 +1,27 @@
-from pydantic_settings import BaseSettings
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class GRPCSettings(BaseSettings):
     ADDRESS: str
 
+    model_config = SettingsConfigDict(
+        env_nested_delimiter="__",
+        env_file_encoding="utf-8",
+        env_file=Path(__file__).parent.parent / ".env",
+        extra="ignore"
+    )
 
 class FastApiSettings(BaseSettings):
     DEBUG: bool
     CORS_ALLOWED_ORIGINS: list[str]
-    
+
+    model_config = SettingsConfigDict(
+        env_nested_delimiter="__",
+        env_file_encoding="utf-8",
+        env_file=Path(__file__).parent.parent / ".env",
+        extra="ignore"
+    )
     
 grpc_settings = GRPCSettings()
 fastapi_settings = FastApiSettings()
